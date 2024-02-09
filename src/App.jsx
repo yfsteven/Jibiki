b98c6b0ca4cb37b2ae91337df59f5df811d0a284
yfsteven <yaninquiry@gmail.com>
2024-02-09 18:37:22 -0500

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import reactLogo from './assets/react.svg'
+import React, { useEffect } from 'react'
 import Header from '../components/Header'
 import Page from '../components/Page'
 
 function App() {
-  const [japaneseWordData, switchToAnotherJapaneseWord] = React.useState({
-      word: "",
-      definition: "",
-      url: ""
-  })
+  const fetchData = async () => {
+    const res = await fetch("https://sakura-paris.org/dict/?api=1&q=携帯&dict=広辞苑&type=0&romaji=1", { mode: 'no-cors'});
+    const data = await res.json();
+    console.log(data);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
 
   return (
     <div className="App">
